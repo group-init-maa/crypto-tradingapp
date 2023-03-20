f67179029f6de12539c2c5f0ae8c031cf8ec3d0c
A-Rifkan <a.rifkan2019@gmail.com>
2023-03-20 15:17:34 +0000

    Set up login page redirects

--- CryptoTrader/login/urls.py
+++ CryptoTrader/login/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index, name="home"),
+    path("signup", views.singup, name="signup"),
+    path("signin", views.signin, name="signin"),
+    path("signout", views.signout, name="signout")
 ]
--- CryptoTrader/login/views.py
+++ CryptoTrader/login/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.core.mail import EmailMessage, send_mail
 # Create your views here.
 
 def index(request):
     responseData = render_to_string("login\login.html")
     return HttpResponse(responseData)
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+    
+    myuser = User.objects.create_user(username, email, password)
+
+    myuser.save()
+    subject = "Welcome to GFG- Django Login!!"
+    message = "Hello " + myuser.first_name + "!! \n" + "Welcome to GFG!! \nThank you for visiting our website\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nAnubhav Madhav"        
+    from_email = settings.EMAIL_HOST_USER
+    to_list = [myuser.email]
+    send_mail(subject, message, from_email, to_list, fail_silently=True)
+    
+    messages.success(request, "Your account has been made ! :3")
+
+
+    return redirect("signin")
+
+def signout(request):
+    return render(request, "login/signout.html")
+
+def singup(request):
+    return render(request, "login/signup.html")
