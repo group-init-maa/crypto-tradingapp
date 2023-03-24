7172644563b104f6ffc1b27e0e6d13f16ca8b748
MaxWCode <maxwardle008@googlemail.com>
2023-03-24 17:23:33 +0000

    Started market charts

--- CryptoTrader/login/chart.py
+++ CryptoTrader/login/chart.py
@@ -1,20 +1,33 @@
 import requests
 import pandas as pd
-import datetime
-import matplotlib.pyplot as plt
+import mplfinance as mpf
 
 #checks if crytpo exists
 
 def availableCrypto():
-    url = f'https://api.coingecko.com/api/v3/coins'
+    url = f'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin%2Csolana&vs_currencies=gbp'
     response = requests.get(url)
     data = response.json()
 
     cryptoids = []
 
     for asset in data:
-        cryptoids.append(asset['id'])
+        cryptoids.append(asset)
 
     return cryptoids
 
-print(availableCrypto())
+
+
+#retrive chart data for curreency
+
+def getMarketCharts(coinId='bitcoin', vsCurrency='gbp', days='max', interval='daily'):
+    cryptoIds = availableCrypto()
+
+    if coinId in cryptoIds:
+        url = f'https://api.coingecko.com/api/v3/coins/{coinId}/market_chart?vs_currency={vsCurrency}&days={days}'
+        response = requests.get(url)
+        data = response.json()
+    
+    return data
+
+print(getMarketCharts())
